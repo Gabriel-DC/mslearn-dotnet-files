9a37dd5e6f67252e080b82d0f86fb8c67a67ea8a
Gabriel-DC <gabrieldcgh@gmail.com>
2023-01-03 22:47:07 -0300

    Tutorial end

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace mslearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+            var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+            Directory.CreateDirectory(salesTotalDir);
+
+            var salesFiles = FindFiles(storesDirectory);
+
+            var salesTotal = CalculateSalesTotal(salesFiles);
+
+            File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+        }
+
+        static IEnumerable<string> FindFiles(string folderName)
+        {
+            List<string> salesFiles = new List<string>();
+
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+            foreach (var file in foundFiles)
+            {
+                var extension = Path.GetExtension(file);
+                if (extension == ".json")
+                    salesFiles.Add(file);
+            }
+
+            return salesFiles;
+        }
+
+        static double CalculateSalesTotal(IEnumerable<string> salesFiles)
+        {
+            double salesTotal = 0;
+
+            foreach (var file in salesFiles)
+            {
+                string salesJson = File.ReadAllText(file);
+
+                SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+                salesTotal += data?.Total ?? 0;
+            }
+
+            return salesTotal;
+        }
+
+        record SalesData(double Total);
+    }
+}
